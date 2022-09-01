5debca561445361a97cf7d8fa67dddbc62806666
Ndeta Innocent <you@example.com>
2022-09-01 12:45:12 +0300

    Drop check

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,25 @@ fn main() {
         func: do_it,
     };
     println!("{}", clo.call());
+    let x: u8;
+    let y: u8;
+    {
+        let x: u8;
+        {
+            let y: u8;
+        }
+    }
+    let tuple: (Vec<u8>, Vec<u8>) = (vec![], vec![]);
+    struct Inspector<'a>(&'a u8);
+    struct World<'a> {
+        inspector: Option<Inspector<'a>>,
+        days: Box<u8>,
+    }
+    let mut world = World {
+        inspector: None,
+        days: Box::new(1),
+    };
+    world.inspector = Some(Inspector(&world.days));
 }
 
 //(THis code does not compile)
